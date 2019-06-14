f19309c753c6bf45549c28d0c399c0d790e20deb
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-06-14 10:48:33 -0500

    second commit

---
+++ filter_ccs_script.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python
+#note: for this script should use my venv called envir0
+import random as rd
+import numpy as np
+import CCS_Filter as cf
+
+path_stub = '/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/'
+start_ccs_path_stub = path_stub + 'no_passes/'
+cells = ['2_B01', '3_C01', '4_D01']
+destination_stub = path_stub + 'profile_hmm/'
+
+#3p barcodes
+barcodes = [cf.barcode_2, cf.barcode_3, cf.barcode_4]
+
+for i in np.arange(3):
+    start_ccs_path = start_ccs_path_stub + cell + '/extracted_ccs.csv'
+    cell = cells[i]
+    barcode = barcodes[i]
+    filtered_filename_path = destination_stub + cell + '_filtered_ccs.csv'
+    cf.filter_ccs(start_ccs_path, filtered_filename_path, barcode)
+    
