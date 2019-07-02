20ee18a3e29aecd913ee6bfc13d994211f1cb1dc
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-07-02 10:47:15 -0500

    finished forward emissions

--- generate_emission_probs.py
+++ generate_emission_probs.py
@@ -37,7 +37,7 @@ bar3emission_dict = {}
 bar4emission_dict = {}
 threeP_emission_lists = []
 adapter_emission_lists = []
-
+cell_emission_list = [{},{},{}] #entry 0 for 2B01, 1 for 3C01, 2 for 4D01
 ###########################
 ###########################
 ###########################
@@ -47,46 +47,71 @@ adapter_emission_lists = []
 ###########################
 
 
-#RNA insert
-rna_insert_emission_dict = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25}
+#RNAinsert and Insert states
+random_dict = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25}
+
+
 
 
 #key: index in sequence (0 for first character)
 #value: emission probs dict for that position  
 
 #5p barcode
-starting_keys = [n for n in np.arange(len(fiveBarcode))]
-fiveP_dict = dict.fromkeys(starting_keys, '!')
 fiveP_emission_list = [0] *len(fiveBarcode)
 for i in np.arange(len(fiveBarcode)):
     fiveP_emission_list[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
     bp = fiveBarcode[i]
     fiveP_emission_list[i][bp] = 0.95
-print(fiveP_emission_list)
 
-# cells = ['2_B01', '3_C01', '4_D01']
-# for i in np.arange(3):
-#     cell = cells[i]
-#     barcode3p = threeP_barcodes[i]
-#     adapter = adapters[i]
-#     adapter_dict = adapter_emission_lists[i]
-#     bar_emission_dict = threeP_emission_lists[i]
-#     #3p barcode:
-#     starting_keys =[n for n in np.arange(len(barcode3p))]
-#     bar_emission_dict = dict.fromkeys(starting_keys, '!')
-#     for i in np.arange(len(barcode3p)):
-#         bar_emission_dict[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
-#         bp = barcode3p[i]
-#         bar_emission_dict[i][bp] = 0.95
-#     #adapter
-#     starting_keys = [n for n in np.arange(len(adapter))]
-#     adapter_dict = dict.fromkeys(starting_keys, '!')
-#     for i in np.arange(len(adapter)):
-#         adapter_dict[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
-#         bp = adapter[i]
-#         adapter_dict[i][bp]=0.95
-#
-#
+cells = ['2_B01', '3_C01', '4_D01']
+for i in np.arange(3):
+    cell = cells[i]
+    barcode3p = threeP_barcodes[i]
+    adapter = adapters[i]
+    #adapter_dict = {}
+    #bar_emission_dict = {}
+    #3p barcode:
+    this_bar_emission_list = [0]*len(barcode3p)
+    for j in np.arange(len(barcode3p)):
+        this_bar_emission_list[j] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
+        bp = barcode3p[j]
+        this_bar_emission_list[j][bp] = 0.95
+    #adapter
+    this_adapter_emission_list = [0]*len(adapter)
+    for i in np.arange(len(adapter)):
+        this_adapter_emission_list[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
+        bp = adapter[i]
+        this_adapter_emission_list[i][bp]=0.95
+
+    #combine into this cell's list of emission dicts
+
+    this_dict = cell_emission_list[i]
+    # 5p barcode
+    for index in np.arange(25): # 0 to 24; 5p barcode
+        match_str = "M" + str(index)
+        insert_str = "I" + str(index)
+        this_dict[match_str] = fiveP_emission_list[index]
+        this_dict[insert_str] = random_dict
+
+    #RNAinsert
+    this_dict["R"] = random_dict
+    this_dict["IR"] = random_dict
+
+    #adapter
+    for index in np.arange(25,70): # 25-69
+        match_str = "M" + str(index)
+        insert_str = "I" + str(index)
+        this_dict[match_str] = this_adapter_emission_list[index]
+        this_dict[insert_str] = random_dict
+
+    #3p barcode
+    for index in np.arange(70,141): #70-140
+        match_str = "M" + str(index)
+        insert_str = "I" + str(index)
+        this_dict[match_str] = this_bar_emission_list[index]
+        this_dict[insert_str] = random_dict
+
+print(cell_emission_list[2])
 #
 # #BACKWARD SEQUENCE COMPONENTS
 # RCthreeP_barcodes = [barcode2rc, barcode3rc, barcode4rc]
