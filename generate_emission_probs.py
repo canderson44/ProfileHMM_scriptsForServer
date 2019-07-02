bacf807363506e4699d0187869a65803f4897499
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-07-02 11:16:48 -0500

    finished backwards emissions

--- generate_emission_probs.py
+++ generate_emission_probs.py
@@ -32,12 +32,13 @@ adapter4rc = ac.four_adapter_RC
 threeP_barcodes = [barcode2, barcode3, barcode4]
 adapters = [adapter2, adapter3, adapter4]
 
-bar2emission_dict = {}
-bar3emission_dict = {}
-bar4emission_dict = {}
-threeP_emission_lists = []
-adapter_emission_lists = []
 cell_emission_list = [] #entry 0 for 2B01, 1 for 3C01, 2 for 4D01
+#key: index in sequence (0 for first character)
+#value: emission probs dict for that position
+
+
+#RNAinsert and Insert states
+random_dict = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25}
 ###########################
 ###########################
 ###########################
@@ -46,15 +47,6 @@ cell_emission_list = [] #entry 0 for 2B01, 1 for 3C01, 2 for 4D01
 ###########################
 ###########################
 
-
-#RNAinsert and Insert states
-random_dict = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25}
-
-
-
-
-#key: index in sequence (0 for first character)
-#value: emission probs dict for that position  
 
 #5p barcode
 fiveP_emission_list = [0] *len(fiveBarcode)
@@ -112,69 +104,89 @@ for i in np.arange(3):
         this_dict[insert_str] = random_dict
     cell_emission_list.append(this_dict)
 
-print(cell_emission_list[2])
-#
-# #BACKWARD SEQUENCE COMPONENTS
-# RCthreeP_barcodes = [barcode2rc, barcode3rc, barcode4rc]
-# RCadapters = [adapter2rc, adapter3rc, adapter4rc]
-# RCbar2emission_dict = {}
-# RCbar3emission_dict = {}
-# RCbar4emission_dict = {}
-# RCthreeP_emission_lists = [0,0,0]
-# RCadapter_emission_lists = [0,0,0]
-#
-# #RNA insert
-# RCrna_insert_emission_dict = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25}
-#
-# #key: index in sequence (0 for first character)
-# #value: emission probs dict for that position
-#
-# #5p barcode rc
-# starting_keys = [n for n in np.arange(len(fiveBarcodeRC))]
-# RCfiveP_dict = dict.fromkeys(starting_keys, '!')
-# for i in np.arange(len(fiveBarcodeRC)):
-#     RCfiveP_dict[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
-#     bp = fiveBarcodeRC[i]
-#     RCfiveP_dict[i][bp] = 0.95
-#
-#
-# cells = ['2_B01', '3_C01', '4_D01']
-# for i in np.arange(3):
-#     cell = cells[i]
-#     RCbarcode3p = RCthreeP_barcodes[i]
-#     RCadapter = RCadapters[i]
-#     RCadapter_dict = RCadapter_emission_lists[i]
-#     RCbar_emission_dict = RCthreeP_emission_lists[i]
-#     #3p barcode:
-#     starting_keys =[n for n in np.arange(len(RCbarcode3p))]
-#     RCbar_emission_dict = dict.fromkeys(starting_keys, '!')
-#     for i in np.arange(len(RCbarcode3p)):
-#         RCbar_emission_dict[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
-#         bp = RCbarcode3p[i]
-#         RCbar_emission_dict[i][bp] = 0.95
-#     #adapter
-#     starting_keys = [n for n in np.arange(len(RCadapter))]
-#     adapter_dict = dict.fromkeys(starting_keys, '!')
-#     for i in np.arange(len(adapter)):
-#         RCadapter_dict[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
-#         bp = RCadapter[i]
-#         RCadapter_dict[i][bp]=0.95
-#
-# #######
-# #######
-# #STATES
-# ######
-# ######
-#
-# #INSERT STATE
-# insertState_emission_dict = {'A':0.25, 'C':0.25, 'G':0.25, 'T':0.25}
-#
-# #SEQUENCE STATE: FORWARD
-# forwardSeqState2B01 = {}
-# forwardSeqState3C01 = {}
-# forwardSeqState4D01 = {}
-#
-# #SEQUENCE STATE: BACKWARD
-# backwardSeqState2B01 = {}
-# backwardSeqState3C01 = {}
-# backwardSeqState4D01 = {}
+###########################
+###########################
+###########################
+#BACKWARD SEQUENCE COMPONENTS
+###########################
+###########################
+###########################
+RCthreeP_barcodes = [barcode2rc, barcode3rc, barcode4rc]
+RCadapters = [adapter2rc, adapter3rc, adapter4rc]
+
+
+
+#5p barcode rc
+#5p barcode
+RCfiveP_emission_list = [0] *len(fiveBarcode)
+for i in np.arange(len(fiveBarcode)):
+    RCfiveP_emission_list[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
+    bp = fiveBarcodeRC[i]
+    RCfiveP_emission_list[i][bp] = 0.95
+
+
+cells = ['2_B01', '3_C01', '4_D01']
+for i in np.arange(3):
+    cell = cells[i]
+    RCbarcode3p = RCthreeP_barcodes[i]
+    RCadapter = RCadapters[i]
+    #3p barcode:
+    this_bar_emission_list = [0]*len(RCbarcode3p)
+    for j in np.arange(len(RCbarcode3p)):
+        this_bar_emission_list[j] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
+        bp = RCbarcode3p[j]
+        this_bar_emission_list[j][bp] = 0.95
+    #adapter
+    this_adapter_emission_list = [0]*len(RCadapter)
+    for i in np.arange(len(RCadapter)):
+        this_adapter_emission_list[i] = {'A':0.05, 'C':0.05, 'G':0.05, 'T':0.05}
+        bp = RCadapter[i]
+        this_adapter_emission_list[i][bp]=0.95
+
+    # combine into this cell's list of emission dicts
+
+    this_dict = cell_emission_list[i]
+    # 3p barcode
+    for index in np.arange(71):  # 0 to 70; 3p barcode
+        match_str = "Mr" + str(index)
+        insert_str = "Ir" + str(index)
+        this_dict[match_str] = this_bar_emission_list[index]
+        this_dict[insert_str] = random_dict
+
+    # adapter
+    for index in np.arange(71, 116):  # 71-115
+        match_str = "Mr" + str(index)
+        insert_str = "Ir" + str(index)
+        this_dict[match_str] = this_adapter_emission_list[index - 71]
+        this_dict[insert_str] = random_dict
+
+    # RNAinsert
+    this_dict["Rr"] = random_dict
+    this_dict["IRr"] = random_dict
+
+    # 5p barcode
+    for index in np.arange(116, 141):  # 116-140
+        match_str = "Mr" + str(index)
+        insert_str = "Ir" + str(index)
+        this_dict[match_str] = RCfiveP_emission_list[index - 116]
+        this_dict[insert_str] = random_dict
+
+
+#Getters.
+#getter
+#returns list of emissions dicts.
+## One dict each for 2B01, 3C01, 4D01 cell lines, in that order
+def get_all_cells_emissions():
+    return cell_emission_list
+#getter
+#returns 2B01's emission dict
+def get_2B01_emissions():
+    return cell_emission_list[0]
+# getter
+#returns 3C01's emission dict
+def get_3C01_emissions():
+    return cell_emission_list[1]
+# getter
+#returns 4D01's emission dict
+def get_4D01_emissions():
+    return cell_emission_list[2]
