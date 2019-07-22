3bbedc3afa5f8239dc7b3b953b59e7bacffe13f1
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-07-21 22:37:45 -0500

    annotation files editing 2019-07-21 34

--- Annotate_CCS.py
+++ Annotate_CCS.py
@@ -238,7 +238,7 @@ def gen_test_strings(barcode_index, barcode, barcodeRC, adapter, adapterRC):
     test_strings.append(threeBar_mutations[0])
 
     #3 sub 3 sub
-    test_strings.append(threeBar_mutations[0] + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[0])
+    test_strings.append(threeBar_mutations[0] + "".join(rd.choices(nucleotides, k=1000)) + threeBar_mutations[0])
     #3sub 3
     test_strings.append(threeBar_mutations[0] + "".join(rd.choices(nucleotides, k=100)) + barcode)
     # #test 3insert 3
