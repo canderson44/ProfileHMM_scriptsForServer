eb5b591ef1b3d1dba7ac090ba30443461d85c0b2
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-07-21 20:41:48 -0500

    annotation files editing 2019-07-21 14

--- Annotate_CCS.py
+++ Annotate_CCS.py
@@ -188,43 +188,43 @@ def gen_test_strings(barcode_index, barcode, barcodeRC, adapter, adapterRC):
     test_ar3r5r = adapterRC + barcodeRC + fivePBarcodeRC
     test_strings.append(test_ar3r5r)
 
-    # #A53A
-    # test_a53a = adapter + "".join(rd.choices(nucleotides, k=50))+ fivePBarcode+\
-    #             "".join(rd.choices(nucleotides, k=100)) + barcode + \
-    #             "".join(rd.choices(nucleotides, k=10)) + adapter
-    # test_strings.append(test_a53a)
-    # #5rA3Ar
-    # test_5ra3ar = fivePBarcodeRC + adapter + "".join(rd.choices(nucleotides, k=10)) +\
-    #               barcode +"".join(rd.choices(nucleotides, k=100)) +adapterRC
-    # test_strings.append(test_5ra3ar)
-    # #start5
-    # test_start5= fivePBarcode + "".join(rd.choices(nucleotides, k=(1000)))
-    # test_strings.append(test_start5)
-    # #A_rand_A
-    # test_a_rand_a = adapter + "".join(rd.choices(nucleotides, k=900)) + adapter
-    # test_strings.append(test_a_rand_a)
-    # #rand
-    # test_random="".join(rd.choices(nucleotides, k=(1000)))
-    # test_strings.append(test_random)
-    # #end in 3
-    # test_endIn3pBarcode= "".join(rd.choices(nucleotides, k=1000)) + barcode
-    # test_strings.append(test_endIn3pBarcode)
-    #
-    # #following check that keep all above threshold, not just highest scoring for some region
-    # #also, since beginning of 3 is 5' barcode exactly, makes sure 5' eliminated, not intended 3'
-    #
-    # #A_Sub3_3
-    # test_aSub3_3 = test_aSub3_3 = adapter + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[0] + "".join(
-    #     rd.choices(nucleotides, k=100)) + barcode
-    # test_strings.append(test_aSub3_3)
-    # #A_Insert3_3
-    # test_aInsert3_3 = adapter + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[1] + "".join(
-    #     rd.choices(nucleotides, k=100)) + barcode
-    # test_strings.append(test_aInsert3_3)
-    # #A_Del3_3
-    # test_aDel3_3 = adapter + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[2] + "".join(
-    #     rd.choices(nucleotides, k=100)) + barcode
-    # test_strings.append(test_aDel3_3)
+    #A53A
+    test_a53a = adapter + "".join(rd.choices(nucleotides, k=50))+ fivePBarcode+\
+                "".join(rd.choices(nucleotides, k=100)) + barcode + \
+                "".join(rd.choices(nucleotides, k=10)) + adapter
+    test_strings.append(test_a53a)
+    #5rA3Ar
+    test_5ra3ar = fivePBarcodeRC + adapter + "".join(rd.choices(nucleotides, k=10)) +\
+                  barcode +"".join(rd.choices(nucleotides, k=100)) +adapterRC
+    test_strings.append(test_5ra3ar)
+    #start5
+    test_start5= fivePBarcode + "".join(rd.choices(nucleotides, k=(1000)))
+    test_strings.append(test_start5)
+    #A_rand_A
+    test_a_rand_a = adapter + "".join(rd.choices(nucleotides, k=900)) + adapter
+    test_strings.append(test_a_rand_a)
+    #rand
+    test_random="".join(rd.choices(nucleotides, k=(1000)))
+    test_strings.append(test_random)
+    #end in 3
+    test_endIn3pBarcode= "".join(rd.choices(nucleotides, k=1000)) + barcode
+    test_strings.append(test_endIn3pBarcode)
+
+    #following check that keep all above threshold, not just highest scoring for some region
+    #also, since beginning of 3 is 5' barcode exactly, makes sure 5' eliminated, not intended 3'
+
+    #A_Sub3_3
+    test_aSub3_3 = test_aSub3_3 = adapter + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[0] + "".join(
+        rd.choices(nucleotides, k=100)) + barcode
+    test_strings.append(test_aSub3_3)
+    #A_Insert3_3
+    test_aInsert3_3 = adapter + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[1] + "".join(
+        rd.choices(nucleotides, k=100)) + barcode
+    test_strings.append(test_aInsert3_3)
+    #A_Del3_3
+    test_aDel3_3 = adapter + "".join(rd.choices(nucleotides, k=100)) + threeBar_mutations[2] + "".join(
+        rd.choices(nucleotides, k=100)) + barcode
+    test_strings.append(test_aDel3_3)
     return test_strings
 
 #given a sequence, and a list of lists of [reference, reference character], 
@@ -483,7 +483,7 @@ def test_annotateseq(ref_list, seq_list):
         print(final_coords)
         print("format: fused string")
         print(final_fused_list)
-        print()
+        print("################\n##############\n##############\n#############\n\n")
 
 
 
