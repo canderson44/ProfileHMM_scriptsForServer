1dcb60566536bde21f31067223960bdfc644cfdb
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-07-25 17:28:15 -0500

    HMM 2019-07-25

--- toyHMM_generate_trainingSequences.py
+++ toyHMM_generate_trainingSequences.py
@@ -13,17 +13,22 @@ Sequences follow emission probs
 
 ''' 
 TRANSITIONS
-start -> M0: 1
+start -> I0: 0.05
+start -> M0: 0.95
+I0 -> I0: 0.5
+I0 -> M0: 0.5
 M0 -> M1: 0.95
-M0 -> D0: 0.05
-D0 -> M2: 1
+M0 -> M2: 0.05
 M1 -> M2: 1
-M2 -> end: 1
+M2 -> I2: 1
+I2 -> I2: 0.80
+I2 -> END: 0.20
 
 EMISSIONS
 M0: 0.95 A, rest 0.05
 M1: 0.95 C, "   "
 M2: 0.95 G, "   "
+I0 and I2 0.25 for each
 '''
 
 
@@ -37,6 +42,7 @@ sequence_names = [str(n) for n in np.arange(num_sequences)]
 ninetyfive_As = ['a'] * 95
 ninetyfive_Cs = ['c'] * 95
 ninetyfive_Gs = ['g'] * 95
+nucleotides = ['A', 'C', 'G', 'T']
 five_As= ['a'] * 5
 five_Cs = ['c'] * 5
 five_Gs = ['g'] * 5
@@ -47,48 +53,59 @@ m1_sample_space = ninetyfive_Cs + five_As + five_Gs + five_Ts
 m2_sample_space = ninetyfive_Gs + five_As + five_Cs + five_Ts
 
 #transition sample spaces (only need m0's since others are all prob 1 to something
-
+sample_space_transition_from_I0 = ['I0', 'M0']
+sample_space_transition_from_I2 = ['I2'] * 80
+sample_space_transition_from_I2 += ['END'] * 20
 sample_space_transition_from_m0 = ['M1'] * 95
 sample_space_transition_from_m0 += ['M2'] * 5 #represents going to D0, then M2
-
+sample_space_startState = ['M0'] * 95
+sample_space_startState += ['I0'] * 5
 #now let's try make a list of sequences
 all_sequences = []
 all_transitions = []
+transition = rd.choice(sample_space_startState)
 for i in np.arange(num_sequences):
-    #always start with M0
     char_list = []
-    transition_list = ['M0']
-    #first char
-    char_list.append(rd.choice(m0_sample_space))
-    #now transition
-    transition = rd.choice(sample_space_transition_from_m0)
-    transition_list.append(transition)
-
+    transition_list = []
     while(transition != 'end'):
-        if transition == 'M1':
+        if transition == 'I0':
+            char_list.append(rd.choice(nucleotides))
+            transition= rd.choice(sample_space_transition_from_I0)
+        elif transition == 'M0':
+            char_list.append(rd.choice(m0_sample_space))
+            transition = rd.choice(sample_space_transition_from_m0)
+        elif transition == 'M1':
             char_list.append(rd.choice(m1_sample_space))
             transition = 'M2'
-        else: #transition to M2
+        elif transition == 'M2': #transition to M2
             char_list.append(rd.choice(m2_sample_space))
-            transition = 'end'
+            transition = 'I2'
+        else: #transition is I2
+            char_list.append(rd.choice(nucleotides))
+            transition = rd.choice(sample_space_transition_from_I2)
         transition_list.append(transition)
 #    print("".join(char_list))
 #    print(transition_list)
 
-    all_sequences.append("".join(char_list))
+    #all_sequences.append("".join(char_list)) #FOR HMMCONVERTER
+    all_sequences.append(" ".join(char_list))
     all_transitions.append("".join(transition))
 
-output_name = '/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/HMMConverter/toyHMM_simple_sequences.txt'
+#output_name = '/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/HMMConverter/toyHMM_simple_sequences.txt'
+output_name = '/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/ToPS/toyHMM_simple.sequences'
 with open(output_name, 'w') as output:
     for index in np.arange(len(all_sequences)):
-        label = '>' + sequence_names[index] + '\n'
-        output.write(label)
+        # label = '>' + sequence_names[index] + '\n' #FOR HMMCONVERTER
+        # output.write(label)                         #FOR HMMCONVERTER
+        label= sequence_names[index] + ': '
 
         # don't make new line if this is the last sequence
         if index == len(all_sequences)-1:
-            to_write_str = all_sequences[index]
+            # to_write_str = all_sequences[index] #FOR HMMCONVERTER
+            to_write_str = label + all_sequences[index]
         else:
-            to_write_str = all_sequences[index] + '\n'
+            # to_write_str = all_sequences[index] + '\n' #FOR HMMCONVERTER
+            to_write_string = label + all_sequences[index] + '\n'
         output.write(to_write_str)
 
 
