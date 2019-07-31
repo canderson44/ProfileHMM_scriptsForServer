74660a02928de741a9bee647ab4eef7055660abb
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-07-31 18:16:34 -0500

    HMM file generation 2019-07-31 21

--- generate_ccs_hmmFile.py
+++ generate_ccs_hmmFile.py
@@ -34,8 +34,8 @@ nucleotides = ['A','C','G','T']
 for i in np.arange(len(cells)):
     cell = cells[i]
     write_filename = path_stub + cell + '_hmm_initial.xml'
-    emission_filename = path_stub + cell + '_emissions_initial.txt'
-    seq_filename = path_stub + cell + '_trainingSequences.txt'
+    emission_filename = cell + '_emissions_initial.txt'
+    seq_filename = cell + '_trainingSequences.txt'
 
     #state names list
     match_names = ["M"+str(n) for n in np.arange(96)]
