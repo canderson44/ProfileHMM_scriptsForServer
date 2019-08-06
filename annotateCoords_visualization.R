e90716c553eca6d6a1bd572753f4a386d260134f
Catherine Anderson <catherineanderson@Catherines-MacBook-Air.local>
2019-08-05 19:50:29 -0500

    annotation 2019-08-5 3

--- annotateCoords_visualization.R
+++ annotateCoords_visualization.R
@@ -3,23 +3,58 @@
 # Counts combinations of regions identified per zmw. Outputs a csv of format RegionCombo, count
 # Goal: determine where Adapter, 3' barcode, 5' barcode, and their reverse complements
 # lie within a given CCS
+# The pipe taken from script by Colin Dewey
 
+#### load packages and data ####
 library(tidyverse)
 
 install.packages("gridExtra")
 library(gridExtra)
-#### Load data ####
+
 #FOR SERVER 
-#coords_2B01 <- read.csv("/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/2_B01_annotation_coords.csv")
-#FOR MY MAC
-coords_2B01 <- read.csv("../data/2_B01_annotation_coords_2019-08-5.csv")
+coords_2B01 <- read.csv("/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/2_B01_annotation_coords.csv")
+coords_3C01 <- read.csv("/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/3_C01_annotation_coords.csv")
+coords_4D01 <- read.csv("/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/4_D01_annotation_coords.csv")
+
+#FOR CATHERINE MAC
+#coords_2B01 <- read.csv("../data/2_B01_annotation_coords_2019-08-5.csv")
 # coords_3C01 <- read.csv("../data/toy_coords_B.csv")
 # coords_4D01 <- read.csv("../data/toy_coords_C.csv")
 
+#### pipe by Colin Dewey ####
+#2_B01
+pattern_counts_2 <- coords_2B01 %>% 
+  filter(region != "CCS") %>%
+  arrange(ZMW, start) %>%
+  group_by(ZMW) %>%
+  summarise(pattern = paste(region, collapse = " ")) %>%
+  count(pattern) %>%
+  arrange(desc(n))
 
-test <- coords_2B01 %>% filter(ZMW <4260000) %>% filter(region != 'CCS') %>% select (ZMW,region) %>% 
-  unite("region_combos",region, sep=" ")
+#3_C01
+pattern_counts_3 <- coords_3C01 %>% 
+  filter(region != "CCS") %>%
+  arrange(ZMW, start) %>%
+  group_by(ZMW) %>%
+  summarise(pattern = paste(region, collapse = " ")) %>%
+  count(pattern) %>%
+  arrange(desc(n))
 
-for (x in seq_along(coords_2B01)){
-  print(coords_2B01[[x]])
-}
+#4_D01
+pattern_counts_4 <- coords_4D01 %>% 
+  filter(region != "CCS") %>%
+  arrange(ZMW, start) %>%
+  group_by(ZMW) %>%
+  summarise(pattern = paste(region, collapse = " ")) %>%
+  count(pattern) %>%
+  arrange(desc(n))
+
+#### save result ####
+# for server
+write_csv(pattern_counts_2, "/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/2_B01_annotationAllRegionCombos.csv")
+write_csv(pattern_counts_3, "/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/3_C01_annotationAllRegionCombos.csv")
+write_csv(pattern_counts_4, "/tier2/deweylab/scratch/ipsc_pacbio/demultiplexing/profile_hmm/annotated_ccs/4_D01_annotationAllRegionCombos.csv")
+#for Catherine Mac
+#write_csv(pattern_counts_2,"../2_B01_annotationRegionCombos.csv")
+
+
